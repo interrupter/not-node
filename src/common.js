7d6b34fc97b12f80fe843c7258e1f70991e7d46f
Aleksander Repin <alex.repin@mail.ru>
2017-08-31 20:41:49 +0400

    v2.7.1

--- src/common.js
+++ src/common.js
@@ -1,29 +1,8 @@
-/*
-	args - array from function input arguments
-		[
-			0 => arg 1
-			1 => arg 2
-			2 => arg 3
-			...
-		]
-	validators -
-		[
-			0 => validator for arg 1
-			1 => validator for arg 2
-		]
-
-*/
-
-exports.validateArgs = function (args, validators) {
-	if (args.length === validators.length) {
-		for (let i in args) {
-			obj[i] = validators[i];
-		}
-		return false;
-	} else {
-		return true;
-	}
-};
+/**
+ * Change first letter case to lower
+ * @param  {string} string input string
+ * @return {string}        result
+ */
 
 exports.firstLetterToLower = function (string) {
 	return string.charAt(0).toLowerCase() + string.slice(1);
