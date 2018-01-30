b07d3ea164f1b237b8c8ee66aadf229971931982
Aleksander Repin <alex.repin@mail.ru>
2018-01-30 18:11:21 +0400

    v3.0.2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ exports.Error = require('./src/error.js');
 exports.Auth = require('./src/auth/auth.js');
 exports.Manifest = require('./src/manifest/manifest.js');
 exports.notApp = require('./src/app.js');
-exports.QueryHelpers = require('./src/queryHelpers.js');
 exports.Versioning = require('./src/model/versioning.js');
 exports.Increment = require('./src/model/increment.js');
 exports.Proto = require('./src/model/proto.js');
