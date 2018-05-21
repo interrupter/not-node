6db57fce76944e7bb455ccde5cd6050aa4ff3258
Aleksander Repin <alex.repin@mail.ru>
2018-05-21 11:26:07 +0400

    common: +firstLetterToUpper

--- src/common.js
+++ src/common.js
@@ -7,3 +7,13 @@
 exports.firstLetterToLower = function (string) {
 	return string.charAt(0).toLowerCase() + string.slice(1);
 };
+
+/**
+ * Change first letter case to higher
+ * @param  {string} string input string
+ * @return {string}        result
+ */
+
+exports.firstLetterToUpper = function (string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+};
