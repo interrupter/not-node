2fdb13519179ac1943ce54fcee8c7d950eac3223
interrupter <alex.repin@mail.ru>
2022-07-03 15:57:12 +0300

    fix: exceptions.http/db constructor params default value

--- src/exceptions/db.js
+++ src/exceptions/db.js
@@ -2,7 +2,7 @@ const { notRequestError } = require("not-error");
 
 //delete wasnt successful, or error, or count of deleted documents dont match requested
 class DBExceptionDeleteWasNotSuccessful extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super("DB Delete Was Not Successful", { code: 505, ...params }, cause);
     }
 }
@@ -11,7 +11,7 @@ module.exports.DBExceptionDeleteWasNotSuccessful =
 
 //delete wasnt successful, bc document is owned by another user
 class DBExceptionDocumentIsNotOwnerByActiveUser extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super(
             "DB Only Owners Could Delete their documents",
             { code: 403, ...params },
--- src/exceptions/http.js
+++ src/exceptions/http.js
@@ -4,7 +4,7 @@ const { notRequestError } = require("not-error");
 
 //form of request is not valid
 class HttpExceptionBadRequest extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super("Bad Request", { code: 400, ...params }, cause);
     }
 }
@@ -12,7 +12,7 @@ module.exports.HttpExceptionBadRequest = HttpExceptionBadRequest;
 
 //user is not authenticated
 class HttpExceptionUnauthorized extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super("Unauthorized", { code: 401, ...params }, cause);
     }
 }
@@ -20,7 +20,7 @@ module.exports.HttpExceptionUnauthorized = HttpExceptionUnauthorized;
 
 //user is too poor to go there
 class HttpExceptionPaymentRequired extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super("Payment Required", { code: 402, ...params }, cause);
     }
 }
@@ -28,7 +28,7 @@ module.exports.HttpExceptionPaymentRequired = HttpExceptionPaymentRequired;
 
 //user identity is known, but he has no right to go there
 class HttpExceptionForbidden extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super("Forbidden", { code: 403, ...params }, cause);
     }
 }
@@ -36,7 +36,7 @@ module.exports.HttpExceptionForbidden = HttpExceptionForbidden;
 
 //requested address is not exists
 class HttpExceptionNotFound extends notRequestError {
-    constructor({ params = {}, cause = null }) {
+    constructor({ params = {}, cause = null } = {}) {
         super("Not Found", { code: 404, ...params }, cause);
     }
 }
