c8e2ecf7e64f0119beae6d2e2564c8ab98b3f033
interrupter <alex.repin@mail.ru>
2022-07-12 21:16:07 +0300

    added: custom generic factories libs

--- src/generic/index.js
+++ src/generic/index.js
@@ -2,3 +2,30 @@ module.exports.GenericLogic = require("./logic.js");
 module.exports.GenericRoute = require("./route.js");
 module.exports.GenericGetByIdForm = require("./form.getById.js");
 module.exports.GenericListAndCountForm = require("./form.listAndCount.js");
+
+const FORMS = {};
+module.exports.setCustomGenericForm = (name, factory) => {
+    FORMS[name] = factory;
+};
+
+module.exports.getCustomGenericForm = (name) => {
+    return FORMS[name];
+};
+
+const LOGICS = {};
+module.exports.setCustomGenericLogic = (name, factory) => {
+    LOGICS[name] = factory;
+};
+
+module.exports.getCustomGenericLogic = (name) => {
+    return LOGICS[name];
+};
+
+const ROUTES = {};
+module.exports.setCustomGenericRoute = (name, factory) => {
+    ROUTES[name] = factory;
+};
+
+module.exports.getCustomGenericRoute = (name) => {
+    return ROUTES[name];
+};
