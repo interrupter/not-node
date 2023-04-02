10251ebdb96931ae93bc06fa7d9a9c4409f41156
interrupter <alex.repin@mail.ru>
2023-04-02 18:10:50 +0300

    added: generic form authorized action, provides input of only user id, ip, user object

---
+++ src/generic/form.authorizedAction.js
@@ -0,0 +1,28 @@
+//DB related validation tools
+const Form = require("../form/form");
+const { firstLetterToUpper } = require("../common");
+//not-node
+const { getIP } = require("../auth");
+//form
+const FIELDS = [
+    ["activeUserId", { required: true }, "not-node//objectId"],
+    ["activeUser", "not-node//requiredObject"],
+    ["ip", "not-node//ip"],
+];
+
+module.exports = ({ MODULE_NAME, actionName }) => {
+    const FORM_NAME = `${MODULE_NAME}:${firstLetterToUpper(actionName)}Form`;
+    return class extends Form {
+        constructor({ app }) {
+            super({ FIELDS, FORM_NAME, app });
+        }
+
+        extract(req) {
+            return {
+                activeUser: req?.user,
+                activeUserId: req?.user._id,
+                ip: getIP(req),
+            };
+        }
+    };
+};
