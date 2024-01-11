1e07fe1ff39120298e78ef3b447823d178def6b8
interrupter <alex.repin@mail.ru>
2024-01-11 13:59:53 +0300

    added: core field

---
+++ src/core/fields/boolean.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ui: {
+        component: "UISwitch",
+        label: "not-node:field_boolean_label",
+    },
+    model: {
+        type: Boolean,
+        default: false,
+        required: true,
+    },
+};
