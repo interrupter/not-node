19814552a14fab011757da1d72f28ae20f96e16c
Aleksandr Repin <admin@appmon.ru>
2024-07-01 19:14:22 +0300

    added: const.js with default paths; added: absolute tmpPath initialization config

---
+++ src/const.js
@@ -0,0 +1,13 @@
+const DEFAULT_PATH_TMP = "../../tmp";
+const DEFAULT_PATH_STATIC = "../static";
+const DEFAULT_PATH_MODULES = "./modules";
+const DEFAULT_PATH_WS = "./ws";
+const DEFAULT_PATH_DB_DUMPS = "../../db.dumps";
+
+module.exports = {
+    DEFAULT_PATH_WS,
+    DEFAULT_PATH_MODULES,
+    DEFAULT_PATH_STATIC,
+    DEFAULT_PATH_TMP,
+    DEFAULT_PATH_DB_DUMPS,
+};
--- src/init/lib/env.js
+++ src/init/lib/env.js
@@ -1,5 +1,12 @@
 const log = require("not-log")(module, "not-node//init//env");
 const { tryDirAsync } = require("../../common");
+const {
+    DEFAULT_PATH_WS,
+    DEFAULT_PATH_MODULES,
+    DEFAULT_PATH_STATIC,
+    DEFAULT_PATH_TMP,
+    DEFAULT_PATH_DB_DUMPS,
+} = require("../../const");
 
 module.exports = class InitENV {
     static getProxyPort(config) {
@@ -39,24 +46,40 @@ module.exports = class InitENV {
         await emit("env.pre", { config, options, master });
         config.set(
             "staticPath",
-            master.getAbsolutePath(config.get("path:static") || "static")
+            master.getAbsolutePath(
+                config.get("path:static", DEFAULT_PATH_STATIC)
+            )
         );
         config.set(
             "modulesPath",
-            master.getAbsolutePath(config.get("path:modules") || "modules")
+            master.getAbsolutePath(
+                config.get("path:modules", DEFAULT_PATH_MODULES)
+            )
         );
+
+        config.set(
+            "tmpPath",
+            master.getAbsolutePath(config.get("path:tmp", DEFAULT_PATH_TMP))
+        );
+
         config.set(
             "dbDumpsPath",
             master.getAbsolutePath(
-                config.get("path:dbDumps") || "../../db.dumps"
+                config.get("path:dbDumps", DEFAULT_PATH_DB_DUMPS)
             )
         );
         config.set("appPath", options.pathToApp);
         config.set("npmPath", options.pathToNPM);
         config.set("fullServerName", InitENV.getFullServerName(config));
         if (config.get("path:ws")) {
-            log?.log("wsPath", master.getAbsolutePath(config.get("path:ws")));
-            config.set("wsPath", master.getAbsolutePath(config.get("path:ws")));
+            log?.log(
+                "wsPath",
+                master.getAbsolutePath(config.get("path:ws", DEFAULT_PATH_WS))
+            );
+            config.set(
+                "wsPath",
+                master.getAbsolutePath(config.get("path:ws", DEFAULT_PATH_WS))
+            );
         }
         await InitENV.checkPaths(master, config);
         await emit("env.post", { config, options, master });
