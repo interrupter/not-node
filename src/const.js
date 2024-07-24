d5b9522c547ffb48c9c44d796804d2df40ebab23
Aleksandr Repin <admin@appmon.ru>
2024-07-24 11:43:55 +0300

    added: manifest action data types library ACTION_DATA_TYPES

--- src/const.js
+++ src/const.js
@@ -4,10 +4,13 @@ const DEFAULT_PATH_MODULES = "./modules";
 const DEFAULT_PATH_WS = "./ws";
 const DEFAULT_PATH_DB_DUMPS = "../../db.dumps";
 
+const ACTION_DATA_TYPES = require("./manifest/const").ACTION_DATA_TYPES;
+
 module.exports = {
     DEFAULT_PATH_WS,
     DEFAULT_PATH_MODULES,
     DEFAULT_PATH_STATIC,
     DEFAULT_PATH_TMP,
     DEFAULT_PATH_DB_DUMPS,
+    ACTION_DATA_TYPES,
 };
