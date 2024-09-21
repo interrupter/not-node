db961f130b16aa546678d69828bf95962c7a48f0
Aleksandr Repin <admin@appmon.ru>
2024-09-21 17:38:14 +0300

    fix: generic manifest removed getByID access rule for not authorized

--- src/generic/manifest.js
+++ src/generic/manifest.js
@@ -135,9 +135,6 @@ module.exports = (
                     {
                         root: true,
                     },
-                    {
-                        auth: false,
-                    },
                 ],
             },
             getRaw: {
