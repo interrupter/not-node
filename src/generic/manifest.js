188bcac0d7450949b295d923afb07b596f890512
Aleksandr Repin <admin@appmon.ru>
2024-10-01 01:39:50 +0300

    fix: generic manifest action data types

--- src/generic/manifest.js
+++ src/generic/manifest.js
@@ -1,5 +1,6 @@
 const ACTION_SIGNATURES = require("../auth/const").ACTION_SIGNATURES;
 const notManifestRouteResultFilter = require("../manifest/result.filter");
+const { ACTION_DATA_TYPES } = require("../manifest/const.js");
 
 const extraActionsBuilder = (
     MODULE_NAME,
@@ -51,7 +52,7 @@ module.exports = (
             create: {
                 method: "PUT",
                 actionSignature: ACTION_SIGNATURES.CREATE,
-                data: ["data"],
+                data: [ACTION_DATA_TYPES.DATA],
                 rules: [
                     {
                         root: true,
@@ -63,7 +64,7 @@ module.exports = (
                 actionSignature: ACTION_SIGNATURES.UPDATE,
                 //добавка к url'у, вид такой :record[название поля без кавычек]
                 postFix: POST_FIX_RECORD_ID,
-                data: ["data"],
+                data: [ACTION_DATA_TYPES.DATA],
                 //должен быть авторизован или нет
                 formData: true,
                 //должен быть суперпользователем или нет
@@ -76,7 +77,11 @@ module.exports = (
             list: {
                 method: "GET",
                 actionSignature: ACTION_SIGNATURES.READ,
-                data: ["pager", "sorter", "filter"],
+                data: [
+                    ACTION_DATA_TYPES.PAGER,
+                    ACTION_DATA_TYPES.SORTER,
+                    ACTION_DATA_TYPES.FILTER,
+                ],
                 rules: [
                     {
                         root: true,
@@ -87,7 +92,13 @@ module.exports = (
                 method: "GET",
                 actionSignature: ACTION_SIGNATURES.READ,
                 postFix: POST_FIX_ACTION_NAME,
-                data: ["pager", "sorter", "filter", "searcher", "return"],
+                data: [
+                    ACTION_DATA_TYPES.PAGER,
+                    ACTION_DATA_TYPES.SORTER,
+                    ACTION_DATA_TYPES.FILTER,
+                    ACTION_DATA_TYPES.SEARCH,
+                    ACTION_DATA_TYPES.RETURN,
+                ],
                 rules: [
                     {
                         root: true,
