b8b1e0aa199cb3bcb1d89c588c4cba314ad389da
Aleksandr Repin <admin@appmon.ru>
2026-02-01 18:10:25 +0300

    fix: secrets moved to dotEnvs

--- tmpl/dirs/project.mjs
+++ tmpl/dirs/project.mjs
@@ -11,8 +11,29 @@ export default {
     pm2: {
         ifArgs: ["pm2"],
     },
-    ".env": {
-        tmpl: "project/env.ejs",
-        args: ["init_root_user", "not_node_monitor", "not_node_reporter"],
+    ".envs": {
+        content: {
+            development: {
+                tmpl: "project/env.ejs",
+                args: [
+                    "init_root_user",
+                    "not_node_monitor",
+                    "not_node_reporter",
+                    "db",
+                    "session",
+                    "secret",
+                    "modules",
+                    "ws",
+                ],
+            },
+            stage: {
+                tmpl: "project/env.ejs",
+                args: [],
+            },
+            production: {
+                tmpl: "project/env.ejs",
+                args: [],
+            },
+        },
     },
 };
